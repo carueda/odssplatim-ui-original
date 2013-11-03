c5829e8efb19972f52c974020bd85463dd5d0f6d
Carlos Rueda <carueda@mbari.org>
2013-11-03 13:51:25 -0800

    fix again the default rest endpoint url in the config

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,9 +1,6 @@
 var odssplatimConfig = {
 
     // odssplatim-rest endpoint URL.
-    // Default value here assumes that service is running on the same host:port.
-//    rest: "../odssplatim-rest"
-//    rest: "http://localhost:8181"
-//    rest: "http://localhost:7070/odssplatim-rest"
-    rest: "http://odss-test.shore.mbari.org:8080/odssplatim-rest"
+    // Default value here assumes that service is running as a sibling of this app
+    rest: "../odssplatim-rest"
 };
