4adc7d728196e0e136265888da4d3c0899a44c8d
Carlos Rueda <carueda@mbari.org>
2013-11-25 09:26:13 -0800

    remove unneeded script

--- buildwar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# simple script to create a war file with the application just to facilitate
-# deployment on tomcat or similar container.
-
-mkdir -p dist
-cd app && jar cf ../dist/odssplatim-ui.war *
